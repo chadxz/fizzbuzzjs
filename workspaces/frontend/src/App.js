93db3ef74154a0feb8d224c52ca95f02c1ad28fb
Chad McElligott <chad.mcelligott@gmail.com>
2024-02-04 12:51:03 -0600

    Scripts

---
+++ workspaces/frontend/src/App.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function App() {
+    return (<>
+      <div>
+        <p>Fizzbuzz.</p>
+      </div>
+    </>);
+}
+exports.default = App;
---
+++ workspaces/frontend/src/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+var client_1 = require("react-dom/client");
+var App_tsx_1 = require("./App.tsx");
+client_1.default.createRoot(document.getElementById('root')).render(<react_1.default.StrictMode>
+    <App_tsx_1.default />
+  </react_1.default.StrictMode>);
